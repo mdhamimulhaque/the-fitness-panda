299093b01c487f9ac0d6e8486ae804ebce23ec40
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-09-29 01:10:13 +0600

    header logo added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from '../../images/logo.png';
 
 
 const Header = () => {
@@ -6,7 +7,8 @@ const Header = () => {
         <header>
             <div className="navbar bg-orange-100">
                 <div className="flex-1">
-                    <a href='/The Fitness Parade' className="btn btn-ghost normal-case text-xl">The Fitness Parade</a>
+                    <img className='w-12' src={logo} alt="" />
+                    <a href='/The Fitness Parade' className="btn btn-ghost normal-case text-2xl">The Fitness Parade</a>
                 </div>
                 <div className="flex-none">
                     <div className="dropdown dropdown-end">
@@ -25,23 +27,6 @@ const Header = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="dropdown dropdown-end">
-                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img src={`https://placeimg.com/80/80/people`} />
-                            </div>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li>
-                                <a className="justify-between">
-                                    Profile
-                                    <span className="badge">New</span>
-                                </a>
-                            </li>
-                            <li><a>Settings</a></li>
-                            <li><a>Logout</a></li>
-                        </ul>
                     </div>
                 </div>
             </div>
