b097d20180e4ee36f2a5c34ac5212b83a7728780
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-09-29 02:31:11 +0600

    add a break part done

--- src/components/TimeZone/TimeZone.jsx
+++ src/components/TimeZone/TimeZone.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import userImg from '../../images/Md Hamimul Haque.jpg';
 
 const TimeZone = () => {
+    const breakeTimes = [10, 20, 30, 40, 50]
     return (
-        <div className="user_Box">
-            <div className="user_img my-8 text-center">
+        <aside className="user_Box">
+            <section className="user_img my-8 text-center">
                 <div className="avatar">
                     <div className="w-24 rounded-full ring  ring-offset-base-100 ring-offset-2">
                         <img src={userImg} />
@@ -15,8 +16,25 @@ const TimeZone = () => {
                     <small className='text-orange-400'>- Front End Web Developer -</small>
                     <p>I'm an experienced Front-End Web developer & Graphic Designer. I'm a hard-working creature, who is passionate about technology, design, and innovation. I like to play with colours and codes.</p>
                 </div>
-            </div>
-        </div>
+            </section>
+            <section className='break_time'>
+                <h2 className='text-lg font-semibold text-orange-400'>Add A Break</h2>
+                <div
+                    className="break_time_display bg-orange-200 p-4 rounded mt-2 flex justify-between"
+                >
+                    {
+                        breakeTimes.map(breakTime => {
+                            return (
+                                <div
+                                    className="time_box rounded h-10 w-10 bg-white leading-10 text-center transition ease-in-out delay-150 cursor-pointer hover:bg-orange-400 hover:text-white">
+                                    {breakTime}
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </section>
+        </aside>
     );
 };
 
