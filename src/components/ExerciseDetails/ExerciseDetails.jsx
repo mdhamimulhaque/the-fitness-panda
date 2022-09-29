fdb4b6776498b65bae251135478266902b658066
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-09-29 13:40:10 +0600

    min update

--- src/components/ExerciseDetails/ExerciseDetails.jsx
+++ src/components/ExerciseDetails/ExerciseDetails.jsx
@@ -16,12 +16,12 @@ const ExerciseDetails = ({ selectPackageTime, showBreakTime }) => {
         <div>
             <div className="exercise_time_container bg-orange-200 p-4 rounded my-4 flex justify-between">
                 <h2 className='font-semibold'>Exercise time</h2>
-                <h2 className='font-semibold'>{selectPackageTime}mn</h2>
+                <h2 className='font-semibold'>{selectPackageTime}min</h2>
             </div>
 
             <div className="break_time_container bg-orange-200 p-4 rounded my-4 flex justify-between">
                 <h2 className='font-semibold'>Break time</h2>
-                <h2 className='font-semibold'>{showBreakTime}mn</h2>
+                <h2 className='font-semibold'>{showBreakTime}min</h2>
             </div>
 
             <button
